d3993f06810a3f4ca284dddab88baa616f62f128
ben bai <benbai123@gmail.com>
2012-01-21 03:01:09 +0800

    add OnePageLimit practice

---
+++ Flow/LimitOnePage/src/test/Maps.java
@@ -0,0 +1,9 @@
+package test;
+
+import java.util.*;
+
+public class Maps {
+
+	public static Map<String, Long> keyMap = new HashMap<String, Long>();
+	public static Map<String, Long> tmpKeyMap = new HashMap<String, Long>();
+}
