646637f78ae07847fed9cb6fd471925c3fa4a611
ben bai <benbai123@gmail.com>
2013-04-30 22:19:17 +0800

    add show validation message in modal window with ZK CE

---
+++ Pattern/MVVM/AdvancedMVVM/src/blog/ben/test/mvvm/formbinding/ShowMessageInModalWinTestVM.java
@@ -0,0 +1,81 @@
+package blog.ben.test.mvvm.formbinding;
+
+import org.zkoss.bind.Binder;
+import org.zkoss.bind.ValidationContext;
+import org.zkoss.bind.Validator;
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.ContextParam;
+import org.zkoss.bind.annotation.ContextType;
+import org.zkoss.bind.annotation.NotifyChange;
+import org.zkoss.bind.sys.BinderCtrl;
+import org.zkoss.bind.validator.AbstractValidator;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.event.MouseEvent;
+
+public class ShowMessageInModalWinTestVM {
+	private Person _person;
+	private Component _formComponent;
+	public Person getPerson () {
+		if (_person == null) {
+			_person = new Person("Ben", "Bai", 123); // fake
+		}
+		return _person;
+	}
+	public String getPersonContent () {
+		return _person.getFirstName() + " " + _person.getLastName() + ", age = " + _person.getAge();
+	}
+	/**
+	 * empty function as a command for
+	 * save in form binding and update personContent
+	 */
+	@Command
+	@NotifyChange("personContent")
+	public void savePerson () {
+	}
+	@Command
+	public void hideModal (@ContextParam(ContextType.TRIGGER_EVENT) MouseEvent event,
+			@ContextParam(ContextType.BINDER) Binder binder) {
+		((BinderCtrl)binder).getValidationMessages().clearMessages(_formComponent);
+		event.getTarget().getParent().setVisible(false);
+	}
+	/**
+	 * get the validator that will do validation while save
+	 * @return
+	 */
+	public Validator getFormValidator(){
+	    return new AbstractValidator() {
+	         
+	        public void validate(ValidationContext ctx) {
+	        	// get value from form context,
+	        	// ctx.getProperties("firstName")[0].getValue() will
+	        	// get the value that bind with @bind(fx.firstName)
+	        	// in zul page
+	        	_formComponent = ctx.getBindContext().getComponent();
+	        	String firstName = (String)ctx.getProperties("firstName")[0].getValue();
+	        	String lastName = (String)ctx.getProperties("lastName")[0].getValue();
+	        	Integer age = (Integer)ctx.getProperties("age")[0].getValue();
+	        	if (firstName == null || firstName.isEmpty()) {
+	        		// put error message into validationMessages map
+	        		addInvalidMessage(ctx, "firstNameContentError", "firstName is required ");
+	        	} else if (firstName.length() < 3) {
+	            	addInvalidMessage(ctx, "firstNameError", "firstName at least 3 chars ");
+	            }
+	            if (lastName == null || lastName.isEmpty()) {
+	        		addInvalidMessage(ctx, "lastNameContentError", "lastName is required");
+	        	} else if (lastName.length() < 3) {
+	            	addInvalidMessage(ctx, "lastNameError", "lastName at least 3 chars ");
+	            }
+	            if (age == null) {
+	            	addInvalidMessage(ctx, "ageContentError", "age is required");
+	            } else {
+		            if (age < 0) {
+		            	addInvalidMessage(ctx, "ageTooSmallError", "age should not be negative");
+		            }
+		            if (age > 130) {
+		            	addInvalidMessage(ctx, "ageTooLargeError", "age should smaller than 130");
+		            }
+	            }
+	        }
+	    };
+	}
+}
