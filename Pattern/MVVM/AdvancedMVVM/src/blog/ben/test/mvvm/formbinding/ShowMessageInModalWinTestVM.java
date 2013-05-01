5f2b83f22e53779a9664f05371b72adcd4e67ff4
ben bai <benbai123@gmail.com>
2013-05-01 10:16:15 +0800

    fine tune

--- Pattern/MVVM/AdvancedMVVM/src/blog/ben/test/mvvm/formbinding/ShowMessageInModalWinTestVM.java
+++ Pattern/MVVM/AdvancedMVVM/src/blog/ben/test/mvvm/formbinding/ShowMessageInModalWinTestVM.java
@@ -1,5 +1,9 @@
 package blog.ben.test.mvvm.formbinding;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.zkoss.bind.BindUtils;
 import org.zkoss.bind.Binder;
 import org.zkoss.bind.ValidationContext;
 import org.zkoss.bind.Validator;
@@ -11,10 +15,13 @@ import org.zkoss.bind.sys.BinderCtrl;
 import org.zkoss.bind.validator.AbstractValidator;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.event.MouseEvent;
+import org.zkoss.zul.ListModel;
+import org.zkoss.zul.ListModelList;
 
 public class ShowMessageInModalWinTestVM {
 	private Person _person;
 	private Component _formComponent;
+	private BinderCtrl _binderCtrl;
 	public Person getPerson () {
 		if (_person == null) {
 			_person = new Person("Ben", "Bai", 123); // fake
@@ -23,6 +30,22 @@ public class ShowMessageInModalWinTestVM {
 	}
 	public String getPersonContent () {
 		return _person.getFirstName() + " " + _person.getLastName() + ", age = " + _person.getAge();
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public ListModel<String> getMessages () {
+		List<String> result = new ArrayList<String>();
+		if (_binderCtrl != null && _formComponent != null
+			&& _binderCtrl.getValidationMessages() != null
+			&& _binderCtrl.getValidationMessages().getMessages(_formComponent) !=  null) {
+			for (String s : _binderCtrl.getValidationMessages().getMessages(_formComponent)) {
+				result.add(s);
+			}
+		}
+		return new ListModelList(result);
 	}
 	/**
 	 * empty function as a command for
@@ -35,7 +58,6 @@ public class ShowMessageInModalWinTestVM {
 	@Command
 	public void hideModal (@ContextParam(ContextType.TRIGGER_EVENT) MouseEvent event,
 			@ContextParam(ContextType.BINDER) Binder binder) {
-		((BinderCtrl)binder).getValidationMessages().clearMessages(_formComponent);
 		event.getTarget().getParent().setVisible(false);
 	}
 	/**
@@ -43,6 +65,7 @@ public class ShowMessageInModalWinTestVM {
 	 * @return
 	 */
 	public Validator getFormValidator(){
+		final ShowMessageInModalWinTestVM vm = this;
 	    return new AbstractValidator() {
 	         
 	        public void validate(ValidationContext ctx) {
@@ -51,30 +74,33 @@ public class ShowMessageInModalWinTestVM {
 	        	// get the value that bind with @bind(fx.firstName)
 	        	// in zul page
 	        	_formComponent = ctx.getBindContext().getComponent();
+	        	_binderCtrl = (BinderCtrl)ctx.getBindContext().getBinder();
 	        	String firstName = (String)ctx.getProperties("firstName")[0].getValue();
 	        	String lastName = (String)ctx.getProperties("lastName")[0].getValue();
 	        	Integer age = (Integer)ctx.getProperties("age")[0].getValue();
 	        	if (firstName == null || firstName.isEmpty()) {
 	        		// put error message into validationMessages map
-	        		addInvalidMessage(ctx, "firstNameContentError", "firstName is required ");
+	        		addInvalidMessage(ctx, "firstName is required ");
 	        	} else if (firstName.length() < 3) {
-	            	addInvalidMessage(ctx, "firstNameError", "firstName at least 3 chars ");
+	            	addInvalidMessage(ctx, "firstName at least 3 chars ");
 	            }
 	            if (lastName == null || lastName.isEmpty()) {
-	        		addInvalidMessage(ctx, "lastNameContentError", "lastName is required");
+	        		addInvalidMessage(ctx, "lastName is required");
 	        	} else if (lastName.length() < 3) {
-	            	addInvalidMessage(ctx, "lastNameError", "lastName at least 3 chars ");
+	            	addInvalidMessage(ctx, "lastName at least 3 chars ");
 	            }
 	            if (age == null) {
-	            	addInvalidMessage(ctx, "ageContentError", "age is required");
+	            	addInvalidMessage(ctx, "age is required");
 	            } else {
 		            if (age < 0) {
-		            	addInvalidMessage(ctx, "ageTooSmallError", "age should not be negative");
+		            	addInvalidMessage(ctx, "age should not be negative");
 		            }
 		            if (age > 130) {
-		            	addInvalidMessage(ctx, "ageTooLargeError", "age should smaller than 130");
+		            	addInvalidMessage(ctx, "age should smaller than 130");
 		            }
 	            }
+	            // notify messages is changed programmatically
+	            BindUtils.postNotifyChange(null, null, vm, "messages");
 	        }
 	    };
 	}
